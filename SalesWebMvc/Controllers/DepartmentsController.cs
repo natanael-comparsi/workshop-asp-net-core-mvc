9af86ded146ae3b6ebc2319c3a13dae5c7b30bdd
Natanael Rebelatto Comparsi <natanaelrc.2012@gmail.com>
2025-05-18 17:44:07 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // Action (Ação) para retornar uma View (Pagina) referente aos departamentos
+        public IActionResult Index()
+        {
+            // Instancia uma lista de departamentos
+            List<Department> list = new List<Department>();
+
+            // Adiciona 2 elementos a lista de departamentos
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            // Os dados que são passados por parametro no método 'View' são os dados que o controlador envia para a view
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        // Definição de propriedades autoimplementadas
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
