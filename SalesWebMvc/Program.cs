dd642452f52262880fbc6b0d35977cd9f7a5a011
Natanael Rebelatto Comparsi <natanaelrc.2012@gmail.com>
2025-06-12 21:31:06 -0300

    Changing theme

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Options;
 using SalesWebMvc.Data;
 using System.Configuration;
 var builder = WebApplication.CreateBuilder(args);
-// Configuração do provider do MySQL 
+// Configuração do provider do MySQL a partir da definição do DbContext
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"),
                      new MySqlServerVersion(new Version(8, 0, 42)), // Deve ser inserida a versão do MySQL utilizada
@@ -31,7 +31,7 @@ app.UseAuthorization();
 
 app.MapStaticAssets();
 
-// Defini��o de padr�o(pattern) do controlador da rota
+// Definição de padrão(pattern) do controlador da rota
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
